2db710c2430c9cfa7002d11aea8ce681f1edcb15
RingSky <zhaozhiyuan95@gmail.com>
2019-11-28 17:26:46 -0500

    Created ground truth base line.

--- LSH.py
+++ LSH.py
@@ -9,10 +9,10 @@ from __future__ import division
 import re
 import numpy as np
 import sys
+from sklearn.metrics import pairwise_distances
 
-
-m = 2101 #data size
-n = 4100 #total words
+m = 3000 #data size
+n = 5100 #total words
 p = 460 #clean words
 k = 5 #preserve word with frequency >=k, this is cleaning
 
@@ -106,80 +106,25 @@ def printPredictedExamples(f,predictedLabel):
 
 #Data preprocess
 print("Loading and processing training data.")
-f = open(r'./trainingData.txt', 'r',encoding = 'utf -8')
+f = open(r'./data.txt', 'r',encoding = 'utf -8')
 labelVector = generatesLabelVector(f)
 f.seek(0)
 dirtyDict = GenerateDirtyDict(f, labelVector)
-trivialWordsSet = createTrivialSet(dirtyDict, 0.6, 0.4)
-dictionary = generateCleanDict(trivialWordsSet,k,dirtyDict)
 print("The uncleaned dictionary contains " + str(len(dirtyDict)+1)+" words.")
-print("The cleaned dictionary contains " + str(len(dictionary)+1)+" words.")
-f.seek(0)
-cleanFeatureMatrix = generateFeatureMatrix(dictionary, p,f)
 f.seek(0)
 dirtyFeatureMatrix = generateFeatureMatrix(dirtyDict, n,f)
 print("Training data is ready.")
 
+sim=pairwise_distances(dirtyFeatureMatrix,metric='jaccard')
 
+dist = np.zeros([m,m], dtype = float)
+for i in range(sim.shape[0]):
+	for j in range(sim.shape[1]):
+		dist[i,j] = 1 - sim[i,j]
 
-#predict on test set
-dirtyData = 0
-if (str(sys.argv[1])) == "dirty":
-	dirtyData = 1
-m = 450 #data size
-print("Loading and preprocessing test data.")
-f = open(r'./testSet.txt', 'r',encoding = 'utf -8')
-labelVectorVal = generatesLabelVector(f)
-f.seek(0)
-cleanFeatureMatrixVal = generateFeatureMatrix(dictionary, p,f)
-f.seek(0)
-dirtyFeatureMatrixVal = generateFeatureMatrix(dirtyDict, n,f)
-print("Test data is ready.")
-from sklearn import tree  #Using decision tree
-clf = tree.DecisionTreeClassifier()
-if dirtyData == 1:
-	print("Training decision tree model with uncleaned data.")
-	clf = clf.fit(dirtyFeatureMatrix, labelVector)
-	predictedLabel = clf.predict(dirtyFeatureMatrixVal)
-	print("Test accuracy is " +str(np.mean((labelVectorVal == predictedLabel)) * 100)+"%.")
-else:
-	print("Training decision tree model with clean data.")
-	clf = clf.fit(cleanFeatureMatrix, labelVector)
-	predictedLabel = clf.predict(cleanFeatureMatrixVal)
-	print("Test accuracy is " +str(np.mean((labelVectorVal == predictedLabel)) * 100)+"%.")
-
-
-#predict on val set
-print("Loading and preprocessing val data.")
-f = open(r'./valSet.txt', 'r',encoding = 'utf -8')
-labelVectorVal = generatesLabelVector(f)
-f.seek(0)
-cleanFeatureMatrixVal = generateFeatureMatrix(dictionary, p,f)
-f.seek(0)
-dirtyFeatureMatrixVal = generateFeatureMatrix(dirtyDict, n,f)
-print("Val data is ready.")
-if dirtyData == 1:
-	predictedLabel = clf.predict(dirtyFeatureMatrixVal)
-	print("Val accuracy is " +str(np.mean((labelVectorVal == predictedLabel)) * 100)+"%.")
-else:
-	predictedLabel = clf.predict(cleanFeatureMatrixVal)
-	print("Val accuracy is " +str(np.mean((labelVectorVal == predictedLabel)) * 100)+"%.")
-
-#predict on training data
-if dirtyData == 1:
-	predictedLabel = clf.predict(dirtyFeatureMatrix)
-	print("Training accuracy is " +str(np.mean((labelVector == predictedLabel)) * 100)+"%.")
-else:
-	predictedLabel = clf.predict(cleanFeatureMatrix)
-	print("Training accuracy is " +str(np.mean((labelVector == predictedLabel)) * 100)+"%.")
-
-
-#print examples
-print("Examples:")
-f = open(r'./testSet.txt', 'r',encoding = 'utf -8')
-printPredictedExamples(f,predictedLabel)
-
-
+print("Jaccard distance:")
+print(dist)
+print(dist.shape)
 
 
 
