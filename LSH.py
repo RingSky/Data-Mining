9c589ba5bfabcba264d0b368f905834e810644b8
RingSky <zhaozhiyuan95@gmail.com>
2019-11-29 21:01:10 -0500

    Update LSH.py

--- LSH.py
+++ LSH.py
@@ -9,10 +9,15 @@ from __future__ import division
 import re
 import numpy as np
 import sys
+from sklearn.metrics import pairwise_distances
+import multiprocessing
 
+import pickle
+from heapq import heapify,heappop,heappush
+import random
 
-m = 2101 #data size
-n = 4100 #total words
+m = 3000 #data size
+n = 5100 #total words
 p = 460 #clean words
 k = 5 #preserve word with frequency >=k, this is cleaning
 
@@ -106,91 +111,97 @@ def printPredictedExamples(f,predictedLabel):
 
 #Data preprocess
 print("Loading and processing training data.")
-f = open(r'./trainingData.txt', 'r',encoding = 'utf -8')
+f = open(r'./data.txt', 'r',encoding = 'utf -8')
 labelVector = generatesLabelVector(f)
 f.seek(0)
 dirtyDict = GenerateDirtyDict(f, labelVector)
-trivialWordsSet = createTrivialSet(dirtyDict, 0.6, 0.4)
-dictionary = generateCleanDict(trivialWordsSet,k,dirtyDict)
 print("The uncleaned dictionary contains " + str(len(dirtyDict)+1)+" words.")
-print("The cleaned dictionary contains " + str(len(dictionary)+1)+" words.")
 f.seek(0)
-cleanFeatureMatrix = generateFeatureMatrix(dictionary, p,f)
-f.seek(0)
-dirtyFeatureMatrix = generateFeatureMatrix(dirtyDict, n,f)
+featureMatrix = generateFeatureMatrix(dirtyDict, n,f)
 print("Training data is ready.")
 
+"""
+dist=pairwise_distances(featureMatrix,metric='jaccard')
+
+sim = np.zeros([m,m], dtype = float)
+for i in range(sim.shape[0]):
+	for j in range(sim.shape[1]):
+		sim[i,j] = 1 - dist[i,j]
+
+print("Jaccard sim:")
+print(sim)
+"""
+#Reference:https://github.com/rahularora/MinHash/blob/master/minhash.py
 
 
-#predict on test set
-dirtyData = 0
-if (str(sys.argv[1])) == "dirty":
-	dirtyData = 1
-m = 450 #data size
-print("Loading and preprocessing test data.")
-f = open(r'./testSet.txt', 'r',encoding = 'utf -8')
-labelVectorVal = generatesLabelVector(f)
-f.seek(0)
-cleanFeatureMatrixVal = generateFeatureMatrix(dictionary, p,f)
-f.seek(0)
-dirtyFeatureMatrixVal = generateFeatureMatrix(dirtyDict, n,f)
-print("Test data is ready.")
-from sklearn import tree  #Using decision tree
-clf = tree.DecisionTreeClassifier()
-if dirtyData == 1:
-	print("Training decision tree model with uncleaned data.")
-	clf = clf.fit(dirtyFeatureMatrix, labelVector)
-	predictedLabel = clf.predict(dirtyFeatureMatrixVal)
-	print("Test accuracy is " +str(np.mean((labelVectorVal == predictedLabel)) * 100)+"%.")
-else:
-	print("Training decision tree model with clean data.")
-	clf = clf.fit(cleanFeatureMatrix, labelVector)
-	predictedLabel = clf.predict(cleanFeatureMatrixVal)
-	print("Test accuracy is " +str(np.mean((labelVectorVal == predictedLabel)) * 100)+"%.")
+k = 25 #Number of hash functions.
+prime = 5101
+
+def findRandomNos(k,prime):
+	randList = []
+	randIndex = random.randint(0, prime -1) 
+	randList.append(randIndex)
+	while k>0:
+		while randIndex in randList:
+			randIndex = random.randint(0, prime-1) 
+	
+		randList.append(randIndex)
+		k = k-1
+	
+	return randList
+
+  
+randomNoA = findRandomNos(k,prime)
+randomNoB = findRandomNos(k,prime)
+
+def getDocLowestShingleID(docIndex):
+	global featureMatrix
+	docLowestShingleID = {}
+	lowestShingleID = []
+	print(docIndex)
+	for x in range(0,k):
+		listFx = []
+		for i in range(len(featureMatrix[docIndex])):
+			if (featureMatrix[docIndex][i] != 0):
+				temp = (randomNoA[x] * i + randomNoB[x]) % prime # temp is a hashed value of a word of a doc.
+				listFx.append(temp)
+		heapify(listFx)
+		lowestShingleID.append(heappop(listFx)) #lowestShingleID stores k lowest word's hashed index in the feature.
+	return lowestShingleID
 
 
-#predict on val set
-print("Loading and preprocessing val data.")
-f = open(r'./valSet.txt', 'r',encoding = 'utf -8')
-labelVectorVal = generatesLabelVector(f)
-f.seek(0)
-cleanFeatureMatrixVal = generateFeatureMatrix(dictionary, p,f)
-f.seek(0)
-dirtyFeatureMatrixVal = generateFeatureMatrix(dirtyDict, n,f)
-print("Val data is ready.")
-if dirtyData == 1:
-	predictedLabel = clf.predict(dirtyFeatureMatrixVal)
-	print("Val accuracy is " +str(np.mean((labelVectorVal == predictedLabel)) * 100)+"%.")
-else:
-	predictedLabel = clf.predict(cleanFeatureMatrixVal)
-	print("Val accuracy is " +str(np.mean((labelVectorVal == predictedLabel)) * 100)+"%.")
+p = multiprocessing.Pool(4)
+docIndecs = [x for x in range(1, 10)]
 
-#predict on training data
-if dirtyData == 1:
-	predictedLabel = clf.predict(dirtyFeatureMatrix)
-	print("Training accuracy is " +str(np.mean((labelVector == predictedLabel)) * 100)+"%.")
-else:
-	predictedLabel = clf.predict(cleanFeatureMatrix)
-	print("Training accuracy is " +str(np.mean((labelVector == predictedLabel)) * 100)+"%.")
+docLowestShingleID = p.map(getDocLowestShingleID, docIndecs)
+p.close()
+p.join()
 
 
-#print examples
-print("Examples:")
-f = open(r'./testSet.txt', 'r',encoding = 'utf -8')
-printPredictedExamples(f,predictedLabel)
+#for doc in docLowestShingleID:
+#  print doc, docLowestShingleID[doc]
 
+def getFileNo(x):
+	if x<10:
+		x = "0" + str(x)
+	else:
+		x = str(x)
+	
+	return x
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+estimateMatrix = []
+for x in range(0,10):
+	doc1LowestShingles = docLowestShingleID[x]
+	col = []
+	for y in range(0,10):
+		doc2LowestShingles = docLowestShingleID[y]
+		count = 0
+		for i in range(0,k):
+			if doc1LowestShingles[i] == doc2LowestShingles[i]:
+				count = count + 1
+	
+		col.append(count/k)
+	estimateMatrix.append(col)
+npEstimateMatrix = np.asarray(estimateMatrix, dtype=np.float32)
+print("Estimated sim:")
+print(npEstimateMatrix)
